4d7e66eddb3df8e0598fb5815c8d84f1b84da3df
busraakay <61781887+busraakay@users.noreply.github.com>
2022-10-18 15:07:43 +0300

    donguler-while-foreach eklendi.

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            //1 den başlayrak konsoldan girilen sayıya kadar sayı dahil ortalma hesaplayıp konsola yazdıran program.
+            Console.WriteLine("Lütfen sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine("Ortalama: " + toplam / sayi);
+
+            // a dan z ye kadar tüm harfleri konsola yazdır.
+            char kar = 'a';
+            while(kar <= 'z'){
+                Console.Write(kar + " ");
+                kar++;
+            }
+
+            // foreach
+            Console.WriteLine("*****Forach*****");
+
+            string[] arabalar = {"bmw", "mercedes", "toyota", "nissan"} ;
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
