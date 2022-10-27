471bfaa80de5d20fdd2c54dd1edf70023a6af498
busraakay <61781887+busraakay@users.noreply.github.com>
2022-10-27 11:39:22 +0300

    sinif-kavrami eklendi

---
+++ sinif-kavrami/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace sinif_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Ayşe";
+            calisan1.Soyad = "Kara";
+            calisan1.No = 16464516;
+            calisan1.Departman = "Insan Kaynaklari";
+
+            calisan1.CalisanBilgileri();
+            Console.WriteLine("*************************");
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Büşra";
+            calisan2.Soyad = "Akay";
+            calisan2.No = 54646464;
+            calisan2.Departman = "Yazilim";
+
+            calisan2.CalisanBilgileri();
+
+        }
+    }
+
+    class Calisan{
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri(){
+            Console.WriteLine("Calisan adi: " + Ad);
+            Console.WriteLine("Calisan soyadi: " + Soyad);
+            Console.WriteLine("Calisan numarasi: " + No);
+            Console.WriteLine("Calisan departmani: " + Departman);
+        }
+    }
+}
