a694f5865ac529ffd9df54958609a1b55bbabeba
busraakay <61781887+busraakay@users.noreply.github.com>
2022-11-04 11:48:11 +0300

    arayuzler-ornek eklendi.

---
+++ arayuzler-ornek/Corolla.cs
@@ -0,0 +1,18 @@
+namespace arayuzler_ornek
+{
+    public class Corolla : IOtomobil{
+
+        public int KacTekerlektenOlusur()
+        {
+            return 4;
+        }
+
+        public Marka HangiMarkaninAraci(){
+            return Marka.Toyota;
+        }
+
+        public Renk StandartRengiNe(){
+            return Renk.Beyaz;
+        }
+    }
+}
---
+++ arayuzler-ornek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace arayuzler_ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Focus focus = new Focus();
+            Console.WriteLine(focus.HangiMarkaninAraci().ToString());
+            Console.WriteLine(focus.StandartRengiNe().ToString());
+            Console.WriteLine(focus.KacTekerlektenOlusur());
+
+            Civic civic = new Civic();
+            Console.WriteLine(civic.HangiMarkaninAraci().ToString());
+            Console.WriteLine(civic.StandartRengiNe().ToString());
+            Console.WriteLine(civic.KacTekerlektenOlusur());
+
+
+        }
+    }
+}
